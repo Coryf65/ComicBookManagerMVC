36378fa547b1662898eddf9dd2bddbb3b12cafd0
Cory <Coryf65@gmail.com>
2018-02-12 21:24:31 -0600

    Created Update to be used within the controller. where you can uipdate and pass the model into the view.

--- ComicBookLibraryManagerWebApp/Controllers/ComicBooksController.cs
+++ ComicBookLibraryManagerWebApp/Controllers/ComicBooksController.cs
@@ -26,10 +26,10 @@ namespace ComicBookLibraryManagerWebApp.Controllers
         }
         public ActionResult Index()
         {
-            // TODO Get the comic books list.
             // Include the "Series" navigation property.
             //var comicBooks = new List<ComicBook>();
 
+            // TODO Get the comic books list.
             var comicBooks = _context.ComicBooks
                     .Include(cb => cb.Series)
                     .OrderBy(cb => cb.Series.Title)
@@ -46,10 +46,10 @@ namespace ComicBookLibraryManagerWebApp.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            // TODO Get the comic book.
             // Include the "Series", "Artists.Artist", and "Artists.Role" navigation properties.
             // var comicBook = new ComicBook();
 
+            // TODO Get the comic book.
             var comicBook = _context.ComicBooks
                     .Include(cb => cb.Series)
                     .Include(cb => cb.Artists.Select(a => a.Artist))
@@ -88,7 +88,9 @@ namespace ComicBookLibraryManagerWebApp.Controllers
                 var comicBook = viewModel.ComicBook;
                 comicBook.AddArtist(viewModel.ArtistId, viewModel.RoleId);
 
-                // TODO Add the comic book.
+                // TODO Add the comic book to the DB.
+                _context.ComicBooks.Add(comicBook);
+                _context.SaveChanges();
 
                 TempData["Message"] = "Your comic book was successfully added!";
 
@@ -109,13 +111,14 @@ namespace ComicBookLibraryManagerWebApp.Controllers
             }
 
             // TODO Get the comic book.
-            var comicBook = new ComicBook();
+            var comicBook = _context.ComicBooks.Where( cb => cb.Id == id).SingleOrDefault();
 
             if (comicBook == null)
             {
                 return HttpNotFound();
             }
 
+            // if a comicbook is found
             var viewModel = new ComicBooksEditViewModel()
             {
                 ComicBook = comicBook
@@ -135,6 +138,37 @@ namespace ComicBookLibraryManagerWebApp.Controllers
                 var comicBook = viewModel.ComicBook;
 
                 // TODO Update the comic book.
+                _context.Entry(comicBook).State = EntityState.Modified; // Changing the state to modified will cause a sql update statement
+                _context.SaveChanges();
+
+                /* But will update all columns and sometimes you might not wwant to do that so in that case: 
+                 * Let's look at a couple of approaches that you can use to avoid EF creating a SQL update statement 
+                 * that updates every table column.
+                 *
+                 * First off, if you're just looking to prevent updating a single column (or handful of columns), 
+                 * you can use an entity's context entry to change a property's IsModified property to false. 
+                 * For example, this line of code would keep the IssueNumber column from being included in the SQL update statement:     
+                 * 
+                 * comicBookEntry.Property("IssueNumber").IsModified = false;
+                 * 
+                 * Another approach is to retrieve the entity from the database 
+                 * (so that the context is tracking it) and then update that entity with the new values 
+                 * from the user's post data. A convenient way of doing that is to use the Controller class' 
+                 * UpdateModel or TryUpdateModel method to update the entity that's retrieved from the database.
+                 * 
+                 * // Creating an anonymous object here in order to keep the shape of the model
+                 *   // the same as the incoming form post data.
+                 *   var comicBookToUpdate = new
+                 *   {
+                 *       ComicBook = _comicBooksRepository.Get(comicBook.Id)
+                 *   };
+                 *   UpdateModel(comicBookToUpdate);
+                 *   Context.SaveChanges();
+                 *   
+                 *   Securtity Flaw to check out
+                 *   https://odetocode.com/blogs/scott/archive/2012/03/11/complete-guide-to-mass-assignment-in-asp-net-mvc.aspx
+                 *   
+                 * */
 
                 TempData["Message"] = "Your comic book was successfully updated!";
 
@@ -154,6 +188,7 @@ namespace ComicBookLibraryManagerWebApp.Controllers
             }
 
             // TODO Get the comic book.
+            //var comicBook = _context.ComicBooks.Where(cb => cb.Id == id).SingleOrDefault();
             // Include the "Series" navigation property.
             var comicBook = new ComicBook();
 
@@ -182,18 +217,18 @@ namespace ComicBookLibraryManagerWebApp.Controllers
         /// <param name="comicBook">The comic book to validate.</param>
         private void ValidateComicBook(ComicBook comicBook)
         {
-            //// If there aren't any "SeriesId" and "IssueNumber" field validation errors...
-            //if (ModelState.IsValidField("ComicBook.SeriesId") &&
-            //    ModelState.IsValidField("ComicBook.IssueNumber"))
-            //{
-            //    // Then make sure that the provided issue number is unique for the provided series.
-            //    // TODO Call method to check if the issue number is available for this comic book.
-            //    if (false)
-            //    {
-            //        ModelState.AddModelError("ComicBook.IssueNumber",
-            //            "The provided Issue Number has already been entered for the selected Series.");
-            //    }
-            //}
+            // If there aren't any "SeriesId" and "IssueNumber" field validation errors...
+            if (ModelState.IsValidField("ComicBook.SeriesId") &&
+                ModelState.IsValidField("ComicBook.IssueNumber"))
+            {
+                // Then make sure that the provided issue number is unique for the provided series.
+                // TODO Call method to check if the issue number is available for this comic book.
+                if (false)
+                {
+                    ModelState.AddModelError("ComicBook.IssueNumber",
+                        "The provided Issue Number has already been entered for the selected Series.");
+                }
+            }
         }
     }
 }
