a5394cd7bd9055b11f320f9a09b45c7548d5c146
Cory Fabian <none>
2018-02-13 16:02:27 -0600

    Added the ability to C.R.U.D Artists into each ComicBook.

--- ComicBookShared/Data/Context.cs
+++ ComicBookShared/Data/Context.cs
@@ -13,14 +13,11 @@ namespace ComicBookShared.Data
         public DbSet<Series> Series { get; set; }
         public DbSet<Artist> Artists { get; set; }
         public DbSet<Role> Roles { get; set; }
+        public DbSet<ComicBookArtist> ComicBookArtists { get; set; } // lets me CRUD to the artist!
 
         public Context()
         {
-            // This call to the SetInitializer method is used 
-            // to configure EF to use our custom database initializer class
-            // which contains our app's database seed data.
-            // Database.SetInitializer(new DatabaseInitializer());
-            // ! Now we get rid of this because we set up the connection Via the Web.Config file
+            
         }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
